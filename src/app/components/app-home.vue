e85c66672b6acc6a0356cb6dddf2f40c30e7f424
cc <664751829@qq.com>
2021-08-26 17:44:14 +0800

    定义应用图标组件

--- src/app/components/app-home.vue
+++ src/app/components/app-home.vue
@@ -1,14 +1,17 @@
 <template>
   <div>欢迎访问宁皓网：）</div>
   <AppLogo />
+  <AppIcon />
 </template>
 
 <script>
-import AppLogo from './app-log.vue';
+import AppLogo from './app-logo.vue';
+import AppIcon from './app-icon.vue';
 
 export default {
   components: {
     AppLogo,
+    AppIcon,
   },
 };
 </script>
