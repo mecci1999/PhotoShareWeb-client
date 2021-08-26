a13b85f22db0cc2831ee7c6c36967a1f23469ba7
cc <664751829@qq.com>
2021-08-26 21:51:58 +0800

    清理app-home组件

--- src/app/components/app-home.vue
+++ src/app/components/app-home.vue
@@ -1,17 +1,3 @@
 <template>
   <div>欢迎访问宁皓网：）</div>
-  <AppLogo />
-  <AppIcon />
 </template>
-
-<script>
-import AppLogo from './app-logo.vue';
-import AppIcon from './app-icon.vue';
-
-export default {
-  components: {
-    AppLogo,
-    AppIcon,
-  },
-};
-</script>
