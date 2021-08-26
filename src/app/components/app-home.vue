ed34e53cb231719324aa9ae12137e38fa0bf02f2
cc <664751829@qq.com>
2021-08-26 17:13:02 +0800

    定义应用标志组件

--- src/app/components/app-home.vue
+++ src/app/components/app-home.vue
@@ -1,3 +1,14 @@
 <template>
   <div>欢迎访问宁皓网：）</div>
+  <AppLogo />
 </template>
+
+<script>
+import AppLogo from './app-log.vue';
+
+export default {
+  components: {
+    AppLogo,
+  },
+};
+</script>
