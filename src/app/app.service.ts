b48b1f0bb02d73b61a0e68d191357a3247499ff0
cc <664751829@qq.com>
2021-08-27 15:39:51 +0800

    定义本地存储服务方法

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -7,3 +7,19 @@ import { API_BASE_URL } from './app.config';
 export const apiHttpClient = axios.create({
   baseURL: API_BASE_URL,
 });
+
+/**
+ * 设置本地存储
+ */
+export const setStroage = (key: string, value: any) => {
+  localStorage.setItem(key, JSON.stringify(value));
+};
+
+/**
+ * 获得本地存储
+ */
+export const getStroage = (key: string) => {
+  const data = localStorage.getItem(key);
+
+  return data ? JSON.parse(key) : null;
+};
