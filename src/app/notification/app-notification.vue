5c89a6b831e7f5064fe69b9c14c5fc9c9620df57
cc <664751829@qq.com>
2021-08-28 22:20:00 +0800

    定义通知提示信息组件（AppNotificationMessage）

--- src/app/notification/app-notification.vue
+++ src/app/notification/app-notification.vue
@@ -1,9 +1,16 @@
 <template>
-  <div class="app-notification">AppNotification</div>
+  <div class="app-notification">
+    <AppNotificationMessage
+      v-for="message in messages"
+      :key="message.id"
+      :message="message"
+    ></AppNotificationMessage>
+  </div>
 </template>
 
 <script>
 import { defineComponent } from 'vue';
+import AppNotificationMessage from './components/app-notification-message.vue';
 
 export default defineComponent({
   name: 'AppNotification',
@@ -17,7 +24,24 @@ export default defineComponent({
    * 数据
    */
   data() {
-    return {};
+    return {
+      messages: [
+        {
+          id: 1,
+          content: '空山新雨后，天气晚来秋。',
+          icon: 'cloud',
+        },
+        {
+          id: 2,
+          content: '树树皆秋色，山山唯落晖。',
+          icon: 'landscape',
+        },
+        {
+          id: 3,
+          content: '人烟寒橘柚，秋色老梧桐。',
+        },
+      ],
+    };
   },
 
   /**
@@ -40,7 +64,9 @@ export default defineComponent({
   /**
    * 使用组件
    */
-  components: {},
+  components: {
+    AppNotificationMessage,
+  },
 });
 </script>
 
---
+++ src/app/notification/components/app-notification-message.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="app-notification-message">
+    <div class="icon">
+      <AppIcon :name="messageIcon"></AppIcon>
+    </div>
+    <div class="content">{{ message.content }}</div>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+import AppIcon from '@/app/components/app-icon.vue';
+
+export default defineComponent({
+  name: 'AppNotificationMessage',
+
+  /**
+   * 属性
+   */
+  props: {
+    message: {
+      type: Object,
+    },
+  },
+
+  /**
+   * 数据
+   */
+  data() {
+    return {};
+  },
+
+  /**
+   * 计算属性
+   */
+  computed: {
+    messageIcon() {
+      return this.message.icon ? this.message.icon : 'info';
+    },
+  },
+
+  /**
+   * 已创建
+   */
+  created() {
+    //
+  },
+
+  /**
+   * 组件方法
+   */
+  methods: {},
+
+  /**
+   * 使用组件
+   */
+  components: { AppIcon },
+});
+</script>
+
+<style scoped></style>
