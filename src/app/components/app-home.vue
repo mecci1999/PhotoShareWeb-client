c36d89c1069302e105c1f1ec849242a225de3b64
cc <664751829@qq.com>
2021-08-31 16:10:31 +0800

    改进：添加body与notification背景

--- src/app/components/app-home.vue
+++ src/app/components/app-home.vue
@@ -1,29 +1,7 @@
 <template>
-  <div>欢迎访问宁皓网：）{{ isLoggedIn }}</div>
-  <button class="button" @click="onClickPushMessageButton">推送信息</button>
+  <div>欢迎访问宁皓网：）</div>
 </template>
 
 <script>
-import { mapGetters, mapActions } from 'vuex';
-
-export default {
-  computed: {
-    ...mapGetters({
-      isLoggedIn: 'auth/isLoggedIn',
-    }),
-  },
-
-  methods: {
-    ...mapActions({
-      pushMessage: 'notification/pushMessage',
-    }),
-
-    onClickPushMessageButton() {
-      this.pushMessage({
-        content: '空山新雨后，天气晚来秋。',
-        icon: 'cloud',
-      });
-    },
-  },
-};
+export default {};
 </script>
