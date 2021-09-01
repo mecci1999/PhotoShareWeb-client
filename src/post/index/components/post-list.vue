4a6998b633eb7bd28e0c4f69868ac95ad0d0ceb3
cc <664751829@qq.com>
2021-09-01 23:12:49 +0800

    内容列表样式

--- src/post/index/components/post-list.vue
+++ src/post/index/components/post-list.vue
@@ -1,6 +1,7 @@
 <template>
-  <div v-if="loading">加载中...</div>
-  <PostListItem v-for="post in posts" :item="post" :key="post.id" />
+  <div class="post-list">
+    <PostListItem v-for="post in posts" :item="post" :key="post.id" />
+  </div>
 </template>
 
 <script>
@@ -32,3 +33,6 @@ export default defineComponent({
   },
 });
 </script>
+<style scoped>
+@import './styles/post-list.css';
+</style>
