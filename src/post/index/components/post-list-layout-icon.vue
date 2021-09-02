847b6d2c6f767d4cb4dda4215c6e4f38a1f2b40e
cc <664751829@qq.com>
2021-09-02 11:20:43 +0800

    定义内容列表布局图标组件

---
+++ src/post/index/components/post-list-layout-icon.vue
@@ -0,0 +1,62 @@
+<template>
+  <i :class="postListLayoutIconClasses" @click="$emit('click', name)"></i>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+import { mapGetters } from 'vuex';
+
+export default defineComponent({
+  name: 'PostListLayoutIcon',
+
+  /**
+   * 属性
+   */
+  props: {
+    name: {
+      type: String,
+    },
+  },
+
+  emits: ['click'],
+
+  /**
+   * 数据
+   */
+  data() {
+    return {};
+  },
+
+  /**
+   * 计算属性
+   */
+  computed: {
+    ...mapGetters({
+      theme: 'layout/theme',
+    }),
+
+    postListLayoutIconClasses() {
+      return ['post-list-layout-icon', this.name, this.theme];
+    },
+  },
+
+  /**
+   * 已创建
+   */
+  created() {
+    //
+  },
+
+  /**
+   * 组件方法
+   */
+  methods: {},
+
+  /**
+   * 使用组件
+   */
+  components: {},
+});
+</script>
+
+<style scoped></style>
