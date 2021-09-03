8c185c5fbb13d799c59e02eb7882db85b9060f99
cc <664751829@qq.com>
2021-09-03 11:11:24 +0800

    设置每页内容项目数量与定义获取内容列表后期处理动作

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,3 +7,8 @@ export const APP_NAME = process.env.VUE_APP_APP_NAME;
  * 服务接口基本地址
  */
 export const API_BASE_URL = process.env.VUE_APP_API_BASE_URL;
+
+/**
+ * 内容分页
+ */
+export const POSTS_PER_PAGE = parseInt(process.env.VUE_APP_POSTS_PER_PAGE, 10);
--- src/post/index/post-index.store.ts
+++ src/post/index/post-index.store.ts
@@ -1,5 +1,5 @@
 import { Module } from 'vuex';
-import { API_BASE_URL } from '../../app/app.config';
+import { API_BASE_URL, POSTS_PER_PAGE } from '../../app/app.config';
 import { apiHttpClient } from '../../app/app.service';
 import { RootState } from '../../app/app.store';
 import { User } from '../../user/show/user-show.store';
@@ -34,6 +34,8 @@ export interface PostIndexStoreState {
   loading: boolean;
   posts: Array<PostListItem>;
   layout: string;
+  nextPage: number;
+  totalPages: number;
 }
 
 export const postIndexStoreModule: Module<PostIndexStoreState, RootState> = {
@@ -43,6 +45,8 @@ export const postIndexStoreModule: Module<PostIndexStoreState, RootState> = {
     loading: false,
     posts: [],
     layout: '',
+    nextPage: 1,
+    totalPages: 1,
   } as PostIndexStoreState,
 
   getters: {
@@ -91,22 +95,55 @@ export const postIndexStoreModule: Module<PostIndexStoreState, RootState> = {
     setLayout(state, data) {
       state.layout = data;
     },
+
+    setTotalPages(state, data) {
+      state.totalPages = data;
+    },
+
+    setNextPage(state, data) {
+      if (data) {
+        state.nextPage = data;
+      } else {
+        state.nextPage++;
+      }
+    },
   },
 
   actions: {
-    async getPosts({ commit }) {
+    async getPosts({ commit, dispatch, state }) {
       commit('setLoading', true);
 
       try {
-        const response = await apiHttpClient.get('/posts');
-        commit('setPosts', response.data);
-        commit('setLoading', false);
+        const response = await apiHttpClient.get(
+          `/posts?page=${state.nextPage}`,
+        );
+
+        dispatch('getPostsPostProcess', response);
 
         return response;
       } catch (error) {
         commit('setLoading', false);
         throw error.response;
       }
+    },
+
+    getPostsPostProcess({ commit, state }, response) {
+      if (state.nextPage === 1) {
+        commit('setPosts', response.data);
+      } else {
+        commit('setPosts', [...state.posts, ...response.data]);
+      }
+
+      commit('setLoading', false);
+
+      const total =
+        response.headers['X-Total-Count'] || response.headers['x-total-count'];
+
+      const totalPages = Math.ceil(total / POSTS_PER_PAGE);
+
+      commit('setTotalPages', totalPages);
+
+      commit('setNextPage');
     },
   },
 };
