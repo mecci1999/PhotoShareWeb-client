58ec7dc41a6577cb88f5ce61de8e962a0c536dbd
cc <664751829@qq.com>
2021-09-04 14:50:49 +0800

    定义内容数据的类型

--- src/post/show/post-show.store.ts
+++ src/post/show/post-show.store.ts
@@ -1,11 +1,32 @@
 import { Module } from 'vuex';
 import { apiHttpClient } from '../../app/app.service';
 import { RootState } from '../../app/app.store';
+import { User } from '../../user/show/user-show.store';
 
 export interface Post {
   id: number;
   title: string;
   content: string;
+  user: User;
+  totalComments: number;
+  totalLikes: number;
+  file: {
+    id: number;
+    width: number;
+    height: number;
+    orientation: string;
+    size: {
+      thumbnail: string;
+      medium: string;
+      large: string;
+    };
+  };
+  tags: [
+    {
+      id: number;
+      name: string;
+    },
+  ];
 }
 
 export interface PostShowStoreState {
@@ -54,7 +75,7 @@ export const postShowStoreModule: Module<PostShowStoreState, RootState> = {
       } catch (error) {
         commit('setLoading', false);
 
-        throw error.response;
+        throw error;
       }
     },
   },
