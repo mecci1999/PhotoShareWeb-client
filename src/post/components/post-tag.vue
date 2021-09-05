23ba9496b23789a9b20dc4efb137e11d058c22fc
cc <664751829@qq.com>
2021-09-05 20:12:54 +0800

    定义内容标签组件

---
+++ src/post/components/post-tag.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="post-tag">
+    <div class="text">
+      <router-link class="link" :to="tagNameLinkTo">
+        {{ tag.name }}
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'PostTag',
+
+  /**
+   * 属性
+   */
+  props: {
+    tag: {
+      type: Object,
+    },
+  },
+
+  /**
+   * 数据
+   */
+  data() {
+    return {};
+  },
+
+  /**
+   * 计算属性
+   */
+  computed: {
+    tagNameLinkTo() {
+      return { name: 'postIndexPopular', query: { tag: this.tag.name } };
+    },
+  },
+
+  /**
+   * 已创建
+   */
+  created() {
+    //
+  },
+
+  /**
+   * 组件方法
+   */
+  methods: {},
+
+  /**
+   * 使用组件
+   */
+  components: {},
+});
+</script>
+
+<style scoped>
+@import './styles/post-tag.css';
+</style>
