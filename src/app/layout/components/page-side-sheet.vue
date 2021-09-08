93deec282ff625fa923312c70147f943aeee5167
cc <664751829@qq.com>
2021-09-08 10:46:09 +0800

    关闭页面侧板

--- src/app/layout/components/page-side-sheet.vue
+++ src/app/layout/components/page-side-sheet.vue
@@ -1,13 +1,15 @@
 <template>
   <div class="page-side-sheet" v-if="sideSheetComponent">
+    <CloseButton @click="onClickCloseButton"></CloseButton>
     <component :is="sideSheetComponent"></component>
   </div>
 </template>
 
 <script>
 import { defineComponent } from 'vue';
-import { mapGetters } from 'vuex';
+import { mapGetters, mapMutations } from 'vuex';
 import UserCommentSideSheet from '@/user/comment/components/user-comment-side-sheet.vue';
+import CloseButton from '@/app/components/close-button.vue';
 
 export default defineComponent({
   name: 'PageSideSheet',
@@ -37,19 +39,46 @@ export default defineComponent({
    * 已创建
    */
   created() {
-    //
+    // 按下Esc键时关闭页面侧板组件
+    if (window) {
+      window.addEventListener('keyup', this.onKeyUpWindow);
+    }
+  },
+
+  /**
+   * 取消挂载
+   */
+  unmounted() {
+    if (window) {
+      window.removeEventListener('keyup', this.onKeyUpWindow);
+    }
   },
 
   /**
    * 组件方法
    */
-  methods: {},
+  methods: {
+    ...mapMutations({
+      resetSideSheet: 'layout/resetSideSheet',
+    }),
+
+    onClickCloseButton() {
+      this.resetSideSheet();
+    },
+
+    onKeyUpWindow(event) {
+      if (event.key === 'Escape') {
+        this.resetSideSheet();
+      }
+    },
+  },
 
   /**
    * 使用组件
    */
   components: {
     UserCommentSideSheet,
+    CloseButton,
   },
 });
 </script>
