689ab26c405576bfadd809cbd6e7b64430127c45
cc <664751829@qq.com>
2021-09-08 23:37:03 +0800

    修正：内容导航路由地址错误

--- src/post/show/post-show.store.ts
+++ src/post/show/post-show.store.ts
@@ -140,14 +140,13 @@ export const postShowStoreModule: Module<PostShowStoreState, RootState> = {
       if (!getters.canNavigateBack) return;
 
       try {
-        const response = await dispatch('getPostById', getters.prevPost.id);
+        const prevPostId = getters.prevPost.id;
+        const response = await dispatch('getPostById', prevPostId);
 
-        if (getters.prevPost) {
-          appRouter.replace({
-            name: 'postShow',
-            params: { postId: getters.prevPost.id },
-          });
-        }
+        appRouter.replace({
+          name: 'postShow',
+          params: { postId: prevPostId },
+        });
 
         return response;
       } catch (error) {
@@ -166,14 +165,13 @@ export const postShowStoreModule: Module<PostShowStoreState, RootState> = {
       }
 
       try {
-        const response = await dispatch('getPostById', getters.nextPost.id);
+        const nextPostId = getters.nextPost.id;
+        const response = await dispatch('getPostById', nextPostId);
 
-        if (getters.nextPost) {
-          appRouter.replace({
-            name: 'postShow',
-            params: { postId: getters.nextPost.id },
-          });
-        }
+        appRouter.replace({
+          name: 'postShow',
+          params: { postId: nextPostId },
+        });
 
         return response;
       } catch (error) {
