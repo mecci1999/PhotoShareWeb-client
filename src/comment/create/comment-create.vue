95a3f0fd80f98ed9c7c95a20b30c4e7f988ede6f
cc <664751829@qq.com>
2021-09-13 23:15:15 +0800

    处理创建评论组件里的注册与登录按钮

--- src/comment/create/comment-create.vue
+++ src/comment/create/comment-create.vue
@@ -107,11 +107,11 @@ export default defineComponent({
     },
 
     onClickRegisterButton() {
-      console.log('register');
+      this.$router.push({ name: 'login' });
     },
 
     onClickLoginButton() {
-      console.log('login');
+      this.$router.push({ name: 'login' });
     },
 
     onKeyDownCommentTextarea(event) {
