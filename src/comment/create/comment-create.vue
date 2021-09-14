c5dc791573f2bf55de303a71ea7385128f451817
cc <664751829@qq.com>
2021-09-14 20:03:21 +0800

    设计 Pill 按钮的样式

--- src/comment/create/comment-create.vue
+++ src/comment/create/comment-create.vue
@@ -10,20 +10,32 @@
         @keydown="onKeyDownCommentTextarea"
       />
       <div class="actions">
-        <button class="button" v-if="currentUser" @click="onClickCancelButton">
+        <button
+          class="button pill"
+          v-if="currentUser"
+          @click="onClickCancelButton"
+        >
           取消
         </button>
-        <button class="button" v-if="currentUser" @click="onClickSubmitButton">
+        <button
+          class="button pill"
+          v-if="currentUser"
+          @click="onClickSubmitButton"
+        >
           发布
         </button>
         <button
-          class="button"
+          class="button pill"
           v-if="!currentUser"
           @click="onClickRegisterButton"
         >
           注册
         </button>
-        <button class="button" v-if="!currentUser" @click="onClickLoginButton">
+        <button
+          class="button pill"
+          v-if="!currentUser"
+          @click="onClickLoginButton"
+        >
           登录
         </button>
       </div>
