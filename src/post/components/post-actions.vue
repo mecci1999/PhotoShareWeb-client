db3fa19410efdc15d34b6cf8e2617a6a14af0603
cc <664751829@qq.com>
2021-09-19 23:17:50 +0800

    准备删除内容按钮

--- src/post/components/post-actions.vue
+++ src/post/components/post-actions.vue
@@ -1,5 +1,12 @@
 <template>
   <div class="post-actions">
+    <button
+      :class="deleteButtonClasses"
+      @click="onClickDeleteButton"
+      v-if="useDeleteButton"
+    >
+      {{ deleteButtonText }}
+    </button>
     <button :class="submitButtonClasses" @click="onClickSubmitButton">
       {{ submitButtonText }}
     </button>
@@ -20,16 +27,22 @@ export default defineComponent({
     size: {
       type: String,
     },
+
+    useDeleteButton: {
+      type: Boolean,
+    },
   },
 
   /**
    * 数据
    */
   data() {
-    return {};
+    return {
+      confirmDelete: false,
+    };
   },
 
-  emits: ['update', 'create'],
+  emits: ['update', 'create', 'delete'],
 
   /**
    * 计算属性
@@ -47,6 +60,14 @@ export default defineComponent({
 
     submitButtonClasses() {
       return ['button', this.size, { outline: this.unsaved }];
+    },
+
+    deleteButtonText() {
+      return this.confirmDelete ? '确定删除' : '删除';
+    },
+
+    deleteButtonClasses() {
+      return ['button', this.size, 'red', { outline: !this.confirmDelete }];
     },
   },
 
@@ -75,6 +96,10 @@ export default defineComponent({
       } else {
         this.$emit('create');
       }
+    },
+
+    onClickDeleteButton() {
+      this.confirmDelete = !this.confirmDelete;
     },
   },
 
--- src/post/create/post-create.vue
+++ src/post/create/post-create.vue
@@ -3,7 +3,12 @@
     <PostTitleField />
     <PostContentField />
     <PostTagField :postId="postId" v-if="postId" />
-    <PostActions @update="submitUpdatePost" @create="submitCreatePost" />
+    <PostActions
+      @update="submitUpdatePost"
+      @create="submitCreatePost"
+      size="large"
+      :useDeleteButton="postId ? true : false"
+    />
   </div>
 </template>
 
