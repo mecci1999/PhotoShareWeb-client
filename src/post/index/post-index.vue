7494f311e91209a188d441e9e88daac8f46fcb2f
cc <664751829@qq.com>
2021-09-28 15:07:47 +0800

    在内容列表页面监听服务端点赞事件

--- src/post/index/post-index.vue
+++ src/post/index/post-index.vue
@@ -8,9 +8,10 @@
 
 <script>
 import { defineComponent } from 'vue';
-import { mapGetters } from 'vuex';
+import { mapGetters, mapMutations } from 'vuex';
 import PostList from './components/post-list.vue';
 import PostListFilters from './components/post-list-filters.vue';
+import { socket } from '@/app/app.service';
 
 export default defineComponent({
   props: {
@@ -29,6 +30,17 @@ export default defineComponent({
     }),
   },
 
+  created() {
+    socket.on('userLikePostCreated', this.onUserLikePostCreated);
+  },
+
+  /**
+   * 取消挂载
+   */
+  unmounted() {
+    socket.off('userLikePostCreated', this.onUserLikePostCreated);
+  },
+
   title() {
     let title;
     if (this.$route.name === 'postIndexPopular') {
@@ -37,6 +49,21 @@ export default defineComponent({
       title = '最近';
     }
     return title;
+  },
+
+  methods: {
+    ...mapMutations({
+      setPostItemTotalLikes: 'post/index/setPostItemTotalLikes',
+    }),
+
+    onUserLikePostCreated({ postId, socketId }) {
+      if (socket.id === socketId) return;
+
+      this.setPostItemTotalLikes({
+        postId,
+        actionType: 'increase',
+      });
+    },
   },
 
   components: {
