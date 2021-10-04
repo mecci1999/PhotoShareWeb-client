e3b339249c70d8a6323cfa74b35b59aa56ad79a6
cc <664751829@qq.com>
2021-10-04 15:31:18 +0800

    定义复选框字段组件

--- src/app/components/checkbox-field.vue
+++ src/app/components/checkbox-field.vue
@@ -1,8 +1,16 @@
 <template>
-  <label class="checkbox-field" for="demo">
-    <input class="input" id="demo" type="radio" name="demo" value="value" />
+  <label class="checkbox-field" :for="id">
+    <input
+      class="input"
+      :id="id"
+      :type="type"
+      :name="name"
+      :value="value"
+      :checked="isChecked"
+      @change="onChangeInput"
+    />
     <div class="icon"></div>
-    <div class="text">选项</div>
+    <div class="text">{{ text }}</div>
   </label>
 </template>
 
@@ -15,7 +23,26 @@ export default defineComponent({
   /**
    * 属性
    */
-  props: {},
+  props: {
+    modelValue: {},
+
+    type: {
+      type: String,
+      default: 'checkbox',
+    },
+
+    name: {
+      type: String,
+    },
+
+    value: {
+      type: String,
+    },
+
+    text: {
+      type: String,
+    },
+  },
 
   /**
    * 数据
@@ -27,7 +54,32 @@ export default defineComponent({
   /**
    * 计算属性
    */
-  computed: {},
+  computed: {
+    id() {
+      return `${this.name}-${this.value}`;
+    },
+
+    isArray() {
+      return Array.isArray(this.modelValue);
+    },
+
+    isChecked() {
+      let isChecked;
+
+      if (this.isArray) {
+        isChecked = this.modelValue.includes(this.value);
+      } else {
+        isChecked = this.modelValue === this.value;
+      }
+
+      return isChecked;
+    },
+  },
+
+  /**
+   * 事件
+   */
+  emits: ['update:modelValue'],
 
   /**
    * 已创建
@@ -39,7 +91,21 @@ export default defineComponent({
   /**
    * 组件方法
    */
-  methods: {},
+  methods: {
+    onChangeInput(event) {
+      let value = event.target.value;
+
+      if (this.isArray) {
+        if (this.isChecked) {
+          value = this.modelValue.filter(item => item != value);
+        } else {
+          value = [...this.modelValue, value];
+        }
+      }
+
+      this.$emit('update:modelValue', value);
+    },
+  },
 
   /**
    * 使用组件
