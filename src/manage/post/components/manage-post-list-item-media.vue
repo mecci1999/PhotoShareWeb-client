2e3eb17430ccdcb7bd7da02e1fda06dd5238d7e0
cc <664751829@qq.com>
2021-10-05 20:35:11 +0800

    在管理内容页面的内容项目上使用内容审核动作组件

--- src/manage/post/components/manage-post-list-item-media.vue
+++ src/manage/post/components/manage-post-list-item-media.vue
@@ -1,14 +1,22 @@
 <template>
   <div class="manage-post-list-item-media">
     <img class="image" :src="image" v-if="image" />
+    <PostAuditAction
+      class="compact"
+      :post="item"
+      @change="onChangeAuditAction"
+      @click.stop
+    ></PostAuditAction>
     <ManagePostStatusAction :post="item" @click.stop />
   </div>
 </template>
 
 <script>
 import { defineComponent } from 'vue';
+import { mapMutations } from 'vuex';
 import { getImageBase64 } from '@/file/file.service';
 import ManagePostStatusAction from '@/manage/post/components/manage-post-status-action.vue';
+import PostAuditAction from '@/post/components/post-audit-action.vue';
 
 export default defineComponent({
   name: 'ManagePostListItemMedia',
@@ -46,13 +54,22 @@ export default defineComponent({
   /**
    * 组件方法
    */
-  methods: {},
+  methods: {
+    ...mapMutations({
+      setPostItem: 'post/index/setPostItem',
+    }),
+
+    onChangeAuditAction(audit) {
+      this.setPostItem({ id: this.item.id, audit });
+    },
+  },
 
   /**
    * 使用组件
    */
   components: {
     ManagePostStatusAction,
+    PostAuditAction,
   },
 });
 </script>
