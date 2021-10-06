571dd01561d3b4948579211528bb26aeb5d71314
cc <664751829@qq.com>
2021-10-06 11:11:19 +0800

    创建仪表盘与仪表盘访问统计组件

---
+++ src/dashboard/access-count/dashboard-access-count.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="dashboard-access-count">
+    DashboardAccessCount
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+import { mapMutations } from 'vuex';
+
+export default defineComponent({
+  name: 'DashboardAccessCount',
+
+  /**
+   * 属性
+   */
+  props: {},
+
+  /**
+   * 数据
+   */
+  data() {
+    return {};
+  },
+
+  /**
+   * 计算属性
+   */
+  computed: {
+  },
+
+  /**
+   * 已创建
+   */
+  created() {
+  // 
+  },
+
+  /**
+   * 组件方法
+   */
+  methods: {
+    ...mapMutations({}),
+  },
+
+  /**
+   * 使用组件
+   */
+  components: {
+  },
+});
+</script>
+
+<style scoped>
+@import './styles/dashboard-access-count.css';
+</style>
---
+++ src/dashboard/dashboard.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="dashboard">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Dashboard',
+
+  /**
+   * 标题
+   */
+  title() {
+    return '仪表盘';
+  },
+
+  /**
+   * 属性
+   */
+  props: {},
+
+  /**
+   * 数据
+   */
+  data() {
+    return {};
+  },
+
+  /**
+   * 计算属性
+   */
+  computed: {},
+
+  /**
+   * 已创建
+   */
+  created() {
+    //
+  },
+
+  /**
+   * 组件方法
+   */
+  methods: {},
+
+  /**
+   * 使用组件
+   */
+  components: {},
+});
+</script>
+
+<style scoped>
+@import './styles/dashboard.css';
+</style>
