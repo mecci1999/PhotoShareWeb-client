fc416b51f69074367526ca04101e72aaf38fa830
cc <664751829@qq.com>
2021-10-09 15:15:25 +0800

    准备微信登录步骤数据

--- src/weixin/login/weixin-login.store.ts
+++ src/weixin/login/weixin-login.store.ts
@@ -2,8 +2,17 @@ import { Module } from 'vuex';
 import { RootState } from '@/app/app.store';
 import { apiHttpClient } from '@/app/app.service';
 
+export interface LoginStep {
+  name: string;
+  title: string;
+  description: string;
+  component?: string;
+}
+
 export interface WeixinLoginStoreState {
+  currentLoginStepName: string;
   loading: boolean;
+  loginSteps: Array<LoginStep>;
 }
 
 export interface WeixinLoginConnectOptions {
@@ -23,15 +32,57 @@ export const weixinLoginStoreModule: Module<
    * 数据
    */
   state: {
+    currentLoginStepName: 'connectAccount',
     loading: false,
+    loginSteps: [
+      {
+        name: 'waitForUserConfimation',
+        title: '微信登录',
+        description: '用微信扫描下面的二维码',
+        component: 'WeixinLoginIframe',
+      },
+      {
+        name: 'connectAccount',
+        title: '关联账户',
+        description: '关联您的微信账户与已创建的账户',
+        component: 'WeixinLoginConnect',
+      },
+      {
+        name: 'verifyAccount',
+        title: '关联已有账户',
+        description: '请输入本站已有用户相关信息并确定关联',
+        component: 'WeixinLoginAccount',
+      },
+      {
+        name: 'createAccount',
+        title: '关联新账户',
+        description: '创建新的账户并与您的微信账户关联',
+        component: 'WeixinLoginAccount',
+      },
+      {
+        name: 'connectAccountCompleted',
+        title: '关联成功',
+        description: '成功关联了您的微信账户与已创建的账户',
+      },
+    ],
   } as WeixinLoginStoreState,
 
   /**
    * 获取器
    */
   getters: {
+    currentLoginStepName(state) {
+      return state.currentLoginStepName;
+    },
+
     loading(state) {
       return state.loading;
+    },
+
+    currentLoginStep(state) {
+      return state.loginSteps.find(
+        step => step.name === state.currentLoginStepName,
+      );
     },
   },
 
@@ -39,6 +90,10 @@ export const weixinLoginStoreModule: Module<
    * 修改器
    */
   mutations: {
+    setCurrentLoginStepName(state, data) {
+      state.currentLoginStepName = data;
+    },
+
     setLoading(state, data) {
       state.loading = data;
     },
