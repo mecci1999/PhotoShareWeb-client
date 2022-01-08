46f1fd19f222d7d922d56ecf8da77c5757c6bbb3
cc <664751829@qq.com>
2022-01-08 23:12:47 +0800

    定义支付方法小图标组件

---
+++ src/payment/components/payment-icon.vue
@@ -0,0 +1,69 @@
+<template>
+  <i :class="paymentIconClasses" :style="paymentIconStyles"></i>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'PaymentIcon',
+
+  /**
+   * 属性
+   */
+  props: {
+    name: {
+      type: String,
+      default: 'wxpay',
+    },
+
+    size: {
+      type: Number,
+      default: 24,
+    },
+  },
+
+  /**
+   * 数据
+   */
+  data() {
+    return {};
+  },
+
+  /**
+   * 计算属性
+   */
+  computed: {
+    paymentIconClasses() {
+      return ['payment-icon', this.name];
+    },
+
+    paymentIconStyles() {
+      return {
+        fontSize: `${this.size}px`,
+      };
+    },
+  },
+
+  /**
+   * 已创建
+   */
+  created() {
+    //
+  },
+
+  /**
+   * 组件方法
+   */
+  methods: {},
+
+  /**
+   * 使用组件
+   */
+  components: {},
+});
+</script>
+
+<style scoped>
+@import './styles/payment-icon.css';
+</style>
