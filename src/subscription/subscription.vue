133ea1b5838d3d926f080bba20be8bec11b4044d
cc <664751829@qq.com>
2022-02-04 23:26:34 +0800

    创建订阅信息组件并切换显示订阅组件

--- src/subscription/subscription.vue
+++ src/subscription/subscription.vue
@@ -1,12 +1,17 @@
 <template>
   <div class="subscription">
-    <SubscriptionSignup />
+    <component
+      v-if="subscriptionComponent"
+      :is="subscriptionComponent"
+    ></component>
   </div>
 </template>
 
 <script>
 import { defineComponent } from 'vue';
+import { mapGetters, mapActions } from 'vuex';
 import SubscriptionSignup from '@/subscription/signup/subscription-signup.vue';
+import SubscriptionInfo from '@/subscription/info/subscription-info.vue';
 
 export default defineComponent({
   name: 'Subscription',
@@ -33,24 +38,48 @@ export default defineComponent({
   /**
    * 计算属性
    */
-  computed: {},
+  computed: {
+    ...mapGetters({
+      validSubscription: 'subscription/info/validSubscription',
+      loading: 'subscription/info/loading',
+    }),
+
+    subscriptionComponent() {
+      let subscriptionComponent = '';
+
+      if (!this.loading && this.validSubscription) {
+        subscriptionComponent = 'SubscriptionInfo';
+      }
+
+      if (!this.loading && !this.validSubscription) {
+        subscriptionComponent = 'SubscriptionSignup';
+      }
+
+      return subscriptionComponent;
+    },
+  },
 
   /**
    * 已创建
    */
   created() {
-    //
+    this.getValidSubscription();
   },
 
   /**
    * 组件方法
    */
-  methods: {},
+  methods: {
+    ...mapActions({
+      getValidSubscription: 'subscription/info/getValidSubscription',
+    }),
+  },
 
   /**
    * 使用组件
    */
   components: {
+    SubscriptionInfo,
     SubscriptionSignup,
   },
 });
