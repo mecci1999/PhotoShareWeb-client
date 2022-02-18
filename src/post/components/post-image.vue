3a23b0d2202be5d3b66e6ee4440e5c9d40d3c466
cc <664751829@qq.com>
2022-02-18 11:54:30 +0800

    定义内容图像组件

---
+++ src/post/components/post-image.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="post-image">
+    <img class="image" :src="imageUrl" />
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+import { APP_BASE_URL } from '@/app/app.config';
+
+export default defineComponent({
+  name: 'PostImage',
+
+  /**
+   * 属性
+   */
+  props: {
+    file: {
+      type: Object,
+    },
+
+    size: {
+      type: String,
+      default: 'thumbnail',
+    },
+  },
+
+  /**
+   * 数据
+   */
+  data() {
+    return {};
+  },
+
+  /**
+   * 计算属性
+   */
+  computed: {
+    imageUrl() {
+      return `${APP_BASE_URL}/files/${this.file.id}/serve?size=${this.size}`;
+    },
+  },
+
+  /**
+   * 已创建
+   */
+  created() {
+    //
+  },
+
+  /**
+   * 组件方法
+   */
+  methods: {},
+
+  /**
+   * 使用组件
+   */
+  components: {},
+});
+</script>
+
+<style scoped>
+@import './styles/post-image.css';
+</style>
