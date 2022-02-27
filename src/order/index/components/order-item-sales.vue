2dee85f35015a50a454005f6774f25c4f1114ade
cc <664751829@qq.com>
2022-02-27 17:46:38 +0800

    定义订单项目销售组件

---
+++ src/order/index/components/order-item-sales.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="order-item-sales">
+    <div class="item">
+      <div class="title">订单数量</div>
+      <div class="value">{{ sales.count }}</div>
+    </div>
+    <div class="item">
+      <div class="title">销售总额</div>
+      <div class="value">{{ sales.totalAmount }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'OrderItemSales',
+
+  /**
+   * 属性
+   */
+  props: {
+    sales: {
+      type: Object,
+    },
+  },
+
+  /**
+   * 数据
+   */
+  data() {
+    return {};
+  },
+
+  /**
+   * 计算属性
+   */
+  computed: {},
+
+  /**
+   * 已创建
+   */
+  created() {
+    //
+  },
+
+  /**
+   * 组件方法
+   */
+  methods: {},
+
+  /**
+   * 使用组件
+   */
+  components: {},
+});
+</script>
+
+<style scoped>
+@import './styles/order-item-sales.css';
+</style>
